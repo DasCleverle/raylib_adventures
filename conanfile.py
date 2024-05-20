5725a0a740070b510bcadfd58bf81bdc4997b4f5
DasCleverle <dascleverle@gmail.com>
2024-05-20 15:41:09 +0200

    Migrate to conanfile.py to simplify initial setup

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMakeToolchain
+
+class CleverlesAttackingRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+    build_policy = "missing"
+
+    def requirements(self):
+        self.requires("raylib/5.0")
+        self.requires("tl-expected/1.1.0")
+        self.requires("tl-optional/1.1.0")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = True
+        tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
+
