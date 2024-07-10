53223178319359d749aab21a7f0061d1130194bf
DasCleverle <dascleverle@gmail.com>
2024-07-10 22:55:45 +0200

    Add functions to manipulate strings containing utf8 sequences

--- src/utils/include/utils/strings.hpp
+++ src/utils/include/utils/strings.hpp
@@ -1,11 +1,13 @@
+#include <cstddef>
+#include <cstdint>
 #include <string>
 #include <vector>
 
-std::vector<std::string> split(std::string const& value, char const delimiter);
+std::vector<std::string> split(std::string const& value, char delimiter);
 
 template<typename I>
 requires(std::same_as<typename std::iterator_traits<I>::value_type, std::string>)
-std::string join(I begin, I end, char const delimiter) {
+std::string join(I begin, I end, char delimiter) {
     std::string result;
 
     if (begin == end) {
@@ -22,3 +24,11 @@ std::string join(I begin, I end, char const delimiter) {
 
     return result;
 }
+
+std::size_t insert_utf8_codepoint(std::string& target, std::size_t index, std::uint32_t codepoint);
+
+std::size_t erase_utf8_codepoint(std::string& target, std::size_t index);
+
+std::size_t find_utf8_boundary(std::string const& str, std::size_t index);
+
+std::size_t rfind_utf8_boundary(std::string const& str, std::size_t index);
